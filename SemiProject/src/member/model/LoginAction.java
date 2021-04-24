9fbb8c4318f852563d87c49cf607a20e77f726c8
kkvv1 <kkvv1@192.168.219.102>
2021-04-24 12:18:57 +0900

    로그인 페이지 매핑값 추가 그리고 로그인.jsp 파일 위치이동 그리고  매핑값 추가에 따른 자바클래스생성

---
+++ SemiProject/src/member/model/LoginAction.java
@@ -0,0 +1,17 @@
+package member.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class LoginAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		
+	}
+
+}
