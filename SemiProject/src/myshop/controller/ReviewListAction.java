cdb78b5c433162b597cdf1699b3bde5a73d1068a
hyeminJang <jhmin07@nate.com>
2021-05-09 04:21:51 +0900

    수정

--- SemiProject/src/myshop/controller/ReviewListAction.java
+++ SemiProject/src/myshop/controller/ReviewListAction.java
@@ -4,15 +4,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import common.controller.AbstractController;
-import member.model.InterMemberDAO;
-import member.model.MemberDAO;
-import member.model.MemberVO;
+
 import myshop.model.InterProductDAO;
 import myshop.model.ProductDAO;
 import myshop.model.ReviewVO;
